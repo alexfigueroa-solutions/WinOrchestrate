1b93d1e542dff7cb0b5d7cb1f78da1ac6b2085f6
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-11 16:10:48 -0700

    fixes create_docker_shortcut.ps1

--- create_docker_shortcut.ps1
+++ create_docker_shortcut.ps1
@@ -20,78 +20,100 @@ function Log-Message {
     }
 }
 
-function Show-Progress {
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]$Status,
-
-        [Parameter(Mandatory=$true)]
-        [int]$PercentComplete
-    )
-
-    Write-Progress -Activity "Docker Desktop Installation" -Status $Status -PercentComplete $PercentComplete
+# Function to determine if Docker Desktop is installed via Chocolatey
+function IsDockerInstalledViaChoco {
+    $installedPackages = choco list 2>&1
+    Log-Message "INFO" "Installed packages via Chocolatey: $installedPackages"
+    return $installedPackages -like "*docker-desktop*"
 }
 
 function Create-DockerDesktopShortcut {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$dockerPath
+    )
+
     $shortcutPath = "$env:USERPROFILE\Desktop\Docker Desktop.lnk"
     $WshShell = New-Object -comObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($shortcutPath)
-    $Shortcut.TargetPath = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+    $Shortcut.TargetPath = $dockerPath
     $Shortcut.Save()
-
-    if (Test-Path $shortcutPath) {
-        Log-Message "INFO" "Docker Desktop shortcut created on Desktop."
-    } else {
-        Log-Message "ERROR" "Failed to create Docker Desktop shortcut."
-    }
+    Log-Message "INFO" "Docker Desktop shortcut created on the Desktop."
 }
 
 function Pin-DockerToTaskbar {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$dockerPath
+    )
+
     $verb = "pin to taskbar"
-    $path = "C:\Program Files\Docker\Docker\Docker Desktop.exe"
     $shell = New-Object -ComObject "Shell.Application"
-    $folder = $shell.Namespace((Get-Item $path).DirectoryName)
-    $item = $folder.ParseName((Get-Item $path).Name)
+    $folder = $shell.Namespace((Get-Item $dockerPath).DirectoryName)
+    $item = $folder.ParseName((Get-Item $dockerPath).Name)
     $item.InvokeVerb($verb)
-    Log-Message "INFO" "Docker Desktop pinned to taskbar."
+    Log-Message "INFO" "Docker Desktop pinned to the taskbar."
 }
 
 function Install-DockerDesktop {
     try {
         choco install docker-desktop -y
-        Log-Message "INFO" "Docker Desktop installed."
-        return $true
+        Log-Message "INFO" "Docker Desktop installation initiated via Chocolatey."
+        Start-Sleep -s 30
+        $chocoInstalled = choco list docker-desktop --local-only 2>&1
+        if ($chocoInstalled -like "*docker-desktop*") {
+            Log-Message "INFO" "Docker Desktop installation verified through Chocolatey."
+            # Search entire system for Docker Desktop post-installation
+            $result = Get-ChildItem -Path "C:\" -Recurse -Filter "Docker Desktop.exe" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+            if ($result) {
+                Log-Message "INFO" "Docker Desktop found at: $result"
+            } else {
+                Log-Message "ERROR" "Unable to locate Docker Desktop after installation attempt."
+            }
+            return $true
+        }
+        else {
+            Log-Message "ERROR" "Docker Desktop installation failed or is incomplete."
+            return $false
+        }
     }
     catch {
-        Log-Message "ERROR" "Failed to install Docker Desktop. $_"
+        Log-Message "ERROR" "Failed to initiate Docker Desktop installation via Chocolatey."
         return $false
     }
 }
 
-function Verify-DockerDesktop {
-    return (Test-Path "C:\Program Files\Docker\Docker\Docker Desktop.exe")
+function Search-ForDockerDesktop {
+    # Attempt to find Docker Desktop installation across potential paths
+    $possiblePaths = @(
+        "C:\Program Files\Docker\Docker\Docker Desktop.exe",
+        "C:\Program Files (x86)\Docker\Docker\Docker Desktop.exe",
+        "C:\Program Files\Docker\Docker Desktop.exe"
+    )
+
+    foreach ($path in $possiblePaths) {
+        if (Test-Path $path) {
+            return $path
+        }
+    }
+
+    # If not found in standard paths, search the entire C:\Program Files directory
+    $result = Get-ChildItem -Path "C:\Program Files" -Recurse -Filter "Docker Desktop.exe" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+    if ($result) {
+        return $result[0]
+    }
+
+    return $null
 }
+# ... [Previous functions remain unchanged]
 
-Write-Host "Script started..." -ForegroundColor Cyan
-
-# Check for admin rights
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "Admin rights required." -ForegroundColor Red
-    Log-Message "ERROR" "This script requires admin rights. Run with administrator privileges."
-    return
-}
-
-Log-Message "INFO" "Script has necessary permissions. Proceeding..."
-
-# Check and Install Chocolatey if not present
+# Main script execution starts here
 Write-Host "Checking for Chocolatey..." -ForegroundColor Cyan
-
-# Check for Chocolatey's executable directly
 $chocoPath = "C:\ProgramData\chocolatey\bin\choco.exe"
 if (Test-Path $chocoPath) {
-    Log-Message "INFO" "Chocolatey is already installed. Proceeding..."
+    Log-Message "INFO" "Chocolatey is already installed at $chocoPath."
 } else {
-    Write-Host "Chocolatey not found. Attempting installation..." -ForegroundColor Yellow
+    Log-Message "WARN" "Chocolatey not found. Attempting installation..."
     try {
         Set-ExecutionPolicy Bypass -Scope Process -Force
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
@@ -99,43 +121,34 @@ if (Test-Path $chocoPath) {
         Log-Message "INFO" "Chocolatey installed successfully!"
     }
     catch {
-        Log-Message "ERROR" "Failed to install Chocolatey. $_"
+        Log-Message "ERROR" "Failed to install Chocolatey."
         return
     }
 }
-# Install and verify Docker Desktop
-Show-Progress "Checking and Installing Docker Desktop..." 0
 
-$maxRetries = 3
-$installationAttempts = 0
-$installationSuccess = $false
-
-while (-not $installationSuccess -and $installationAttempts -lt $maxRetries) {
-    $isDockerInstalled = choco list --local-only --exact 'docker-desktop' 2>&1
-    if ($isDockerInstalled -notlike "*docker-desktop*") {
-        if ($installationAttempts -eq 0) { # Only try to install once if not detected
-            $installationSuccess = Install-DockerDesktop
-            $installationAttempts++
-        } else {
-            Log-Message "ERROR" "Docker Desktop installation failed on the first attempt. Not retrying."
-            break
-        }
-    } else {
-        Log-Message "INFO" "Docker Desktop is already installed."
-        $installationSuccess = $true
-    }
-    Show-Progress "Installing Docker Desktop..." (($installationAttempts / $maxRetries) * 100)
+# Check if Docker Desktop is installed via Chocolatey
+if (IsDockerInstalledViaChoco) {
+    Log-Message "INFO" "Docker Desktop is installed via Chocolatey."
+} else {
+    Log-Message "WARN" "Docker Desktop not found on the system via Chocolatey."
+    Log-Message "INFO" "Docker Desktop installation initiated via Chocolatey."
+    Install-DockerDesktop
 }
 
-if ($installationSuccess) {
-    Write-Host "Docker Desktop installed successfully." -ForegroundColor Green
-    Log-Message "INFO" "Docker Desktop installed successfully."
-    Create-DockerDesktopShortcut
-    Pin-DockerToTaskbar
+# After installation, verify if Docker Desktop is installed via Chocolatey
+if (IsDockerInstalledViaChoco) {
+    Log-Message "INFO" "Docker Desktop is installed via Chocolatey after installation attempt."
+    $dockerPath = Search-ForDockerDesktop
+    if ($dockerPath) {
+        Log-Message "INFO" "Docker Desktop found at: $dockerPath"
+        Create-DockerDesktopShortcut -dockerPath $dockerPath
+        Pin-DockerToTaskbar -dockerPath $dockerPath
+    } else {
+        Log-Message "ERROR" "Unable to locate Docker Desktop executable after installation attempt."
+    }
 } else {
-    Write-Host "Failed to install Docker Desktop after $maxRetries attempts." -ForegroundColor Red
-    Log-Message "ERROR" "Failed to install Docker Desktop after $maxRetries attempts."
+    Log-Message "ERROR" "Unable to determine the installation of Docker Desktop via Chocolatey after installation attempt."
 }
 
 Log-Message "INFO" "Script execution completed!"
-Write-Host "Script execution"
+Write-Host "Script execution completed!"
